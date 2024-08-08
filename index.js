30013f05d87793153ab7d65f0b35cdc554d81ec7
erikstiven <erikquisnia@gmail.com>
2024-08-08 02:15:38 -0500

    Conexion a la base de datos y configuracion de las variables de entorno

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from 'express'
 import usuarioRoutes from './routes/usuarioRoutes.js'
+import db from './config/db.js'
 
 // Crear la app
 const app = express()
+
+//Conectar a la base de datos
+try {
+  await db.authenticate()
+  console.log('Base de datos conectada')
+} catch (error) {
+  console.log(error)
+}
 
 //habilitar Pug
 app.set('view engine', 'pug')
